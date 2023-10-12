4cf6b4e8bd9f6b9c676a810c8d971c771a3295ae
Potemaruu <mhiroonda@outlook.com>
2023-10-12 15:55:56 +0900

    etc

--- Assets/Script/FlickScript.cs
+++ Assets/Script/FlickScript.cs
@@ -67,5 +67,55 @@ public class FlickScript : MonoBehaviour
             }
             carObject = null;
         }
+
+
+        // スマートフォン上での操作
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                // タッチ開始時の処理
+                var _ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(_ray, out _hit))
+                {
+                    if (_hit.collider != null && _hit.collider.gameObject.CompareTag("Car"))
+                    {
+                        // 当たったオブジェクトのタグがCarなら取得
+                        carObject = _hit.collider.gameObject;
+                    }
+                }
+                touchPos = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved && carObject != null && Vector2.Distance(new Vector3(touchPos.x, touchPos.y, 0.0f), Input.mousePosition) > 20.0f)
+            {
+                // スワイプの方向を取得
+                Vector2 releasePos = touch.position;
+                Vector2 vec = releasePos - touchPos;
+
+                // カメラの向きを考慮する
+                Quaternion rotation = Quaternion.Euler(0f, -cameraY, 0f);
+                Vector3 rotad = rotation * carObject.transform.forward;
+
+                // Vector2に変換
+                Vector2 fo;
+                fo.x = rotad.x;
+                fo.y = rotad.z;
+
+                Car car = carObject.GetComponent<Car>();
+
+                // forwardとの内積が0以上なら前に進める
+                if (Vector2.Dot(vec.normalized, fo.normalized) > 0)
+                {
+                    car.moveVec = carObject.transform.forward * 20.0f;
+                }
+                else
+                {
+                    car.moveVec = -carObject.transform.forward * 20.0f;
+                }
+                carObject = null;
+            }
+        }
     }
 }
